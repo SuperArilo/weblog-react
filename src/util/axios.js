9f52e9441a2976d1d8d07de4b5a7acd18d9aee87
SuperArilo <you@example.com>
2023-01-09 16:36:28 +0800

    更改请求限制时间

--- src/util/axios.js
+++ src/util/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 axios.defaults.withCredentials = false
 const service = axios.create({
     baseURL: 'http://localhost:3090/api',
-    timeout: 120000
+    timeout: 10000
 })
 service.interceptors.request.use( config => {
     if(localStorage.getItem('token') === null){
