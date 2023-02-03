078c811b50fd58f4cb74b8067b3149b0d0fec702
SuperArilo <you@example.com>
2023-02-03 16:01:37 +0800

    添加菜单组件

---
+++ src/components/Menu.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState, useRef } from 'react'
+//组件
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Icon from './Icon'
+//样式
+import style from '../assets/scss/components/menu.module.scss'
+export default function AsukaMenu(props) {
+
+    return (
+        <Menu
+            anchorEl={props.targetElement}
+            open={props.open}
+            onClose={() => { props.onClose(false) }}>
+            {
+                props.renderObject.map(item => {
+                    return (
+                        <MenuItem
+                            disableGutters={false}
+                            key={item.id}
+                            onClick={() => { props.onClickItem(item.id) }}>
+                            {
+                                item.iconClass === ''? '':<Icon iconClass={item.iconClass} />
+                            }
+                            <span className={style.asuka_menu_span}>
+                                {item.title}
+                            </span>
+                        </MenuItem>
+                        
+                    )
+                })
+            }
+      </Menu>
+    )
+}
+AsukaMenu.defaultProps = {
+    targetElement: null,
+    open: false,
+    onClose: () => { return null },
+    onClickItem: () => { return null },
+    renderObject: [
+        {
+            id: 0,
+            title: '未设置',
+            iconClass: ''
+        }
+    ]
+}
