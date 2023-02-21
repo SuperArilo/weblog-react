3e8afcd40370a9b21946197177a5b25f86580ca9
SuperArilo <you@example.com>
2023-02-21 15:37:06 +0800

    微调样式和逻辑

--- src/components/asukaButton.jsx
+++ src/components/asukaButton.jsx
@@ -6,14 +6,29 @@ export default function AsukaButton(props) {
     return (
         <button
             onClick={props.onClick}
-            className={`${style.asuka_button} ${props.class === 'normal' ? style.asuka_button_normal:''} ${props.class === 'read' ? style.asuka_button_read:''} ${props.class === 'danger' ? style.asuka_button_danger:''} ${props.size === 'normal' ? style.button_size_normal:''} ${props.size === 'big' ? style.button_size_big:''} ${props.size === 'small' ? style.button_size_small:''}`}
-            title='提交'
+            className={`${style.asuka_button} ${props.class === 'normal' ? style.asuka_button_normal:''} ${props.class === 'read' ? style.asuka_button_read:''} ${props.class === 'danger' ? style.asuka_button_danger:''} ${props.class === 'file' ? style.asuka_button_file:''} ${props.size === 'normal' ? style.button_size_normal:''} ${props.size === 'big' ? style.button_size_big:''} ${props.size === 'small' ? style.button_size_small:''}`}
+            title={props.class === 'file' ? '点击上传':'提交'}
             type='button'>
             { 
                 props.status ? 
                 <i className={`${'asukamis loading'} ${props.status ? style.rotate:''}`} />
                 :
                 props.text
+            }
+            {
+                props.class === 'file' ? 
+                <label>
+                    <input
+                        type='file'
+                        accept="image/*"
+                        onChange={e => {
+                            let files = {...e.target.files}
+                            props.getFile(files[0])
+                            e.target.value = ''
+                        }}/>
+                </label>
+                :
+                null
             }
             <WaterWave
                 color={`${props.class === 'normal' ? 'rgba(0, 0, 0, 0.7)':''} ${props.class === 'read' ? 'rgb(228, 177, 177)':''}`}
@@ -25,5 +40,8 @@ AsukaButton.defaultProps = {
     text: '未设置',
     status: false,
     class: 'normal',
-    size: 'normal'
+    size: 'normal',
+    getFile: () => {
+        return null
+    }
 }
