adfc42be58e513a780901a8bc8215024310eb908
SuperArilo <you@example.com>
2023-07-14 11:26:50 +0800

    将类组件改成函数式组件

--- src/components/GossipSkeleton.jsx
+++ src/components/GossipSkeleton.jsx
@@ -4,77 +4,46 @@ import style from '../assets/scss/components/gossipSkeleton.module.scss'
 //组件
 import Skeleton from '@mui/material/Skeleton'
 
-export default class GossipSkeleton extends React.Component {
-    render() {
-        return (
-            <div className={style.gossip_skeleton_list}>
-                <div className={style.gossip_skeleton}>
-                    <div className={style.gossip_skeleton_top}>
-                        <Skeleton variant="circular" width='2.8rem' height='2.8rem' />
-                        <div className={style.gossip_skeleton_info}>
-                            <Skeleton variant="text" sx={{ fontSize: '1rem' }} width='8rem' height='2rem' />
-                            <Skeleton variant="text" sx={{ fontSize: '0.5rem' }} width='4rem' height='2rem' />
-                        </div>
-                    </div>
-                    <Skeleton variant="rounded" width='100%' height='3rem' />
-                    <Skeleton variant="text" sx={{ fontSize: '1rem' }} width='6rem' height='2rem' />
-                    <div className={style.gossip_skeleton_bottom}>
-                        <div>
-                            <Skeleton variant="rounded" width='100%' height='2rem' />
-                        </div>
-                        <div>
-                            <Skeleton variant="rounded" width='100%' height='2rem' />
-                        </div>
-                        <div>
-                            <Skeleton variant="rounded" width='100%' height='2rem' />
-                        </div>
+export default function GossipSkeleton(props) {
+    return (
+        <div className={style.gossip_skeleton_list}>
+            <GossipSkeletonItem />
+            {
+                props.viewUid === null &&
+                <>
+                    <GossipSkeletonItem />
+                    <GossipSkeletonItem />
+                </>
+            }
+        </div>
+    )
+}
+const GossipSkeletonItem = () => {
+    return (
+        <div className={style.gossip_skeleton}>
+                <div className={style.gossip_skeleton_top}>
+                    <Skeleton variant="circular" width='2.8rem' height='2.8rem' />
+                    <div className={style.gossip_skeleton_info}>
+                        <Skeleton variant="text" sx={{ fontSize: '1rem' }} width='8rem' height='2rem' />
+                        <Skeleton variant="text" sx={{ fontSize: '0.5rem' }} width='4rem' height='2rem' />
                     </div>
                 </div>
-                <div className={style.gossip_skeleton}>
-                    <div className={style.gossip_skeleton_top}>
-                        <Skeleton variant="circular" width='2.8rem' height='2.8rem' />
-                        <div className={style.gossip_skeleton_info}>
-                            <Skeleton variant="text" sx={{ fontSize: '1rem' }} width='8rem' height='2rem' />
-                            <Skeleton variant="text" sx={{ fontSize: '0.5rem' }} width='4rem' height='2rem' />
-                        </div>
+                <Skeleton variant="rounded" width='100%' height='3rem' />
+                <Skeleton variant="text" sx={{ fontSize: '1rem' }} width='6rem' height='2rem' />
+                <div className={style.gossip_skeleton_bottom}>
+                    <div>
+                        <Skeleton variant="rounded" width='100%' height='2rem' />
                     </div>
-                    <Skeleton variant="rounded" width='100%' height='3rem' />
-                    <Skeleton variant="text" sx={{ fontSize: '1rem' }} width='6rem' height='2rem' />
-                    <div className={style.gossip_skeleton_bottom}>
-                        <div>
-                            <Skeleton variant="rounded" width='100%' height='2rem' />
-                        </div>
-                        <div>
-                            <Skeleton variant="rounded" width='100%' height='2rem' />
-                        </div>
-                        <div>
-                            <Skeleton variant="rounded" width='100%' height='2rem' />
-                        </div>
+                    <div>
+                        <Skeleton variant="rounded" width='100%' height='2rem' />
                     </div>
-                </div>
-                <div className={style.gossip_skeleton}>
-                    <div className={style.gossip_skeleton_top}>
-                        <Skeleton variant="circular" width='2.8rem' height='2.8rem' />
-                        <div className={style.gossip_skeleton_info}>
-                            <Skeleton variant="text" sx={{ fontSize: '1rem' }} width='8rem' height='2rem' />
-                            <Skeleton variant="text" sx={{ fontSize: '0.5rem' }} width='4rem' height='2rem' />
-                        </div>
-                    </div>
-                    <Skeleton variant="rounded" width='100%' height='3rem' />
-                    <Skeleton variant="text" sx={{ fontSize: '1rem' }} width='6rem' height='2rem' />
-                    <div className={style.gossip_skeleton_bottom}>
-                        <div>
-                            <Skeleton variant="rounded" width='100%' height='2rem' />
-                        </div>
-                        <div>
-                            <Skeleton variant="rounded" width='100%' height='2rem' />
-                        </div>
-                        <div>
-                            <Skeleton variant="rounded" width='100%' height='2rem' />
-                        </div>
+                    <div>
+                        <Skeleton variant="rounded" width='100%' height='2rem' />
                     </div>
                 </div>
             </div>
-        )
-    }
+    )
+}
+GossipSkeleton.defaultProps = {
+    viewUid: null
 }
