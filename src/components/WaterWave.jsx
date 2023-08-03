d1083adfb08484c4062e365ffae4ee8b58f87492
SuperArilo <you@example.com>
2023-08-03 16:14:01 +0800

    添加点击波纹效果

---
+++ src/components/WaterWave.jsx
@@ -0,0 +1,39 @@
+import { useCallback, useEffect, useRef } from "react"
+import style from '../assets/scss/components/waterWave.module.scss'
+export default function WaterWave(props) {
+
+    const instanceRef = useRef(null)
+
+    
+
+    const clickFunc = useCallback(e => {
+        let x = e.clientX - instanceRef.current.getBoundingClientRect().left
+        let y = e.clientY - instanceRef.current.getBoundingClientRect().top
+        let ripples = document.createElement('span')
+        ripples.style.top = y + 'px'
+        ripples.style.left = x + 'px'
+        ripples.style.backgroundColor = props.color
+        ripples.style.animationName = style.wave_animate
+        ripples.style.animationDuration = `${props.duration}s`
+        ripples.style.animationTimingFunction = 'linear'
+        ripples.style.animationIterationCount = 'infinite'
+        ripples.className = style.wave_btn
+        instanceRef.current.appendChild(ripples)
+        setTimeout(() => {
+            ripples.remove()
+        }, props.duration * 1000)
+    }, [props.color, props.duration])
+
+    useEffect(() => {
+        const instance = instanceRef.current
+        instance.parentElement.style.position = 'relative'
+        instance.parentElement.addEventListener('click',  clickFunc)
+        return () => instance.parentElement.removeEventListener('click', clickFunc)
+    }, [clickFunc])
+
+    return <aside className={style.wave_aside} ref={instanceRef}/>
+}
+WaterWave.defaultProps = {
+    color: 'rgba(0, 0, 0, 0.7)',
+    duration: 1
+}
