7adfff3c58b4ba6153108109eb54db098c4166c5
SuperArilo <you@example.com>
2023-08-04 10:42:24 +0800

    重写分页组件

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,32 +1,30 @@
 import React, { useState, useEffect, useCallback, useRef, useMemo } from 'react'
 //组件
-import Paginations from "react-js-pagination"
+import Paginations from '@mui/material/Pagination'
 //样式
 import style from '../assets/scss/components/pagination.module.scss'
 export default function Pagination(props) {
 
+    useEffect(() => {
+        console.log(props)
+    }, [props])
+
     return (
-        <div className={style.pagination}>
+        <div className={style.pagination} style={{ justifyContent: props.position === 'center' ? 'center':props.position === 'left' ? 'flex-start':props.position === 'right' ? 'flex-end':'center' }}>
             <Paginations
-                itemClass={style.pagination_item}
-                activeClass={style.pagination_item_selected}
-                activeLinkClass={style.pagination_item_selected_link}
-                prevPageText={<i className='fas fa-angle-left' />}
-                nextPageText={<i className='fas fa-angle-right' />}
-                activePage={props.current}
-                itemsCountPerPage={10}
-                totalItemsCount={props.total}
-                pageRangeDisplayed={props.pageRangeDisplayed}
-                hideFirstLastPages={true}
-                onChange={e => { props.onPageChange(e) }}/>
+                count={props.count}
+                page={props.page}
+                color="primary"
+                size='small'
+                onChange={(event, num) => props.onPageChange(num)}/>
         </div>
     )
 }
 Pagination.defaultProps = {
-    total: 0,
-    current: 0,
-    pageRangeDisplayed: 3,
-    onPageChange: (e) => {
+    page: 0,
+    count: 0,
+    position: 'center',
+    onPageChange: () => {
         return null
     }
 }
