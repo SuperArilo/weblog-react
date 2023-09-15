ff674516a6f8cf7ee80e75f383c9a9aee4ad90a1
SuperArilo <you@example.com>
2023-09-15 16:44:15 +0800

    添加分享api

---
+++ src/util/share.js
@@ -0,0 +1,13 @@
+const share = ({ title, prefx, text }, callBack) => {
+    if(navigator.share === undefined) {
+        callBack('浏览器不支持此组件API')
+        return
+    }
+    navigator.share({
+        title: title,
+        url: `http://192.168.1.2/${prefx}`,
+        text: text
+    })
+    callBack('成功')
+}
+export default share
