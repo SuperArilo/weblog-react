fcd90128be15f3311c5d70d16513c8ec08a294db
SuperArilo <you@example.com>
2023-09-19 09:59:05 +0800

    添加阴影配置项

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -3,28 +3,29 @@ import style from '../assets/scss/components/avatar.module.scss'
 export default function Avatar(props) {
     return (
         <div
+            className={`${style.avatar} ${props.shadow && style.has_shadow}`}
+            style={
+                {
+                    'width': props.width,
+                    'height': props.height,
+                    'minHeight': props.height,
+                    'minWidth': props.width
+                }
+            }>
+            <img
+                onClick={props.onClick}
                 className={style.avatar}
-                style={
-                    {
-                        'width': props.width,
-                        'height': props.height,
-                        'minHeight': props.height,
-                        'minWidth': props.width
-                    }
-                }>
-                <img
-                    onClick={props.onClick}
-                    className={style.avatar}
-                    src={props.src}
-                    title={props.title}
-                    alt={props.alt}/>
-            </div>
+                src={props.src}
+                title={props.title}
+                alt={props.alt}/>
+        </div>
     )
 }
 Avatar.defaultProps = {
     width: '2.5rem',
     height: '2.5rem',
     src: undefined,
+    shadow: false,
     title: '未设置',
     alt: '未设置'
 }
