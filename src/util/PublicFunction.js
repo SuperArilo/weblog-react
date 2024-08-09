40d5d001ed51eeb47258003a025fe817ea0ed639
SuperArilo <superarilowh@gmail.com>
2024-08-09 16:17:44 +0800

    添加功能函数

--- src/util/PublicFunction.js
+++ src/util/PublicFunction.js
@@ -16,3 +16,13 @@ export const ConvertToFormData = obj => {
     }
     return formData
 }
+//生成固定范围的随机数
+export const RandomBetween = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
+//生成随机颜色
+export const RandomRGB = () => {
+    return [
+        Math.floor(Math.random() * 256),
+        Math.floor(Math.random() * 256),
+        Math.floor(Math.random() * 256)
+    ]
+}
